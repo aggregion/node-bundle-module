b562fca68e5ec37dab2518406f34ff5a046c6bde
Андрей Погорельцев < >
2016-12-29 12:25:43 +0300

    alpha 0.2

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-// process.env.LD_LIBRARY_PATH = __dirname + '/build/Release/';
-
-var rms = require('./build/Release/pfileworker.node');
-rms.setOption('verbose', 'off');
-
-module.exports = {
-  createPFileAsync: function(data, cb) {
-    try {
-      var _data = JSON.stringify(data);
-    } catch(e) {
-      return cb(e);
-    }
-
-    return rms.createPFileAsync(_data, cb);
-  }
-};
+var bundles = require('./build/Release/BundlesAddon.node');
+var bundle = new bundles.Bundle("someBundle.dat", ["Read", "Write", "OpenAlways"]);
